2528d0e3786df9a5f9ca1f92ba1c198dcca45869
alex.kovalevv <alex.kovalevv@gmail.com>
2019-08-17 04:56:39 +0300

    Незначительные исправления

--- admin/boot.php
+++ admin/boot.php
@@ -17,86 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Notice that the plugin has been seriously updated!
- *
- * @since 3.0.1
- *
- * @param string $plugin_name
- *
- * @param array  $notices   all registered notices
- *
- * @return array all notices
- */
-function wbcr_ga_admin_conflict_notices_error( $notices, $plugin_name ) {
-	if ( defined( 'LOADING_GA_CACHE_AS_ADDON' ) || $plugin_name != WGA_Plugin::app()->getPluginName() ) {
-		return $notices;
-	}
-
-	$text = '<p>' . __( 'The <b>Simple Google Analytics</b> plugin has some major changes!', 'simple-google-analytics' ) . '</p>';
-	$text .= '<p>' . __( 'Unfortunately, the old version of the plugin (2.2.2) is no longer supported, but you still can download it from the WordPress repository in case if the new release doesn’t work for you.', 'simple-google-analytics' ) . '</p>';
-	$text .= '<p>' . __( 'We’ve updated the code and fixed the compatibility issue for the latest WordPress and PHP versions. We’ve also added additional feature of the Local Google Analytics – this way your website will load faster. The plugin’s name has been changed to Local Google Analytics, but all features remained the same.', 'simple-google-analytics' ) . '</p>';
-	$text .= '<p>' . sprintf( __( 'Please, check <a href="%s">plugin settings</a> and its performance on your website. We do care about you and want to avoid any problems with the new version.', 'simple-google-analytics' ) . '</p>', admin_url( 'options-general.php?page=ga_cache-' . WGA_Plugin::app()->getPluginName() ) ) . '</p>';
-	$text .= '<p>' . sprintf( __( 'We are aimed to pay more attention to the speed and security aspects of your website. That’s why you should definitely try our basic WordPress optimization plugin as well. Clearfy includes functionality of this plugin and has many additional features for the website optimization:
-<a href="%s">Donwload Clearfy for free</a>', 'simple-google-analytics' ), 'https://clearfy.pro?utm_source=wordpress.org&utm_campaign=' . WGA_Plugin::app()->getPluginName() ) . '</p>';
-
-	$notices[] = [
-		'id'              => 'ga_plugin_upgrade_notice1',
-		'type'            => 'warning',
-		'dismissible'     => true,
-		'dismiss_expires' => 0,
-		'text'            => $text
-	];
-
-	return $notices;
-}
-
-add_filter( 'wbcr/factory/admin_notices', 'wbcr_ga_admin_conflict_notices_error', 10, 2 );
-
-/**
- * Migrate settings from the old plugin to the new one.
- *
- * @since 3.0.1
- * @return void
- */
-function wbcr_ga_upgrade() {
-	global $wpdb;
-
-	if ( defined( 'LOADING_GA_CACHE_AS_ADDON' ) ) {
-		return;
-	}
-
-	$is_migrate_up_to_230 = WGA_Plugin::app()->getPopulateOption( 'is_migrate_up_to_230', false );
-
-	if ( ! $is_migrate_up_to_230 ) {
-		$old_plugin_tracking_id              = get_option( 'sga_analytics_id' );
-		$old_plugin_code_location            = get_option( 'sga_code_location' );
-		$old_plugin_demographic_and_interest = (int) get_option( 'sga_demographic_and_interest' );
-		$old_plugin_sga_render_when_loggedin = (int) get_option( 'sga_render_when_loggedin' );
-
-		if ( ! empty( $old_plugin_tracking_id ) ) {
-			WGA_Plugin::app()->updatePopulateOption( 'ga_cache', 1 );
-			WGA_Plugin::app()->updatePopulateOption( 'ga_tracking_id', $old_plugin_tracking_id );
-
-			$script_position = 'footer';
-
-			if ( $old_plugin_code_location == 'head' ) {
-				$script_position = 'header';
-			}
-
-			WGA_Plugin::app()->updatePopulateOption( 'ga_script_position', $script_position );
-			WGA_Plugin::app()->updatePopulateOption( 'ga_anonymize_ip', $old_plugin_demographic_and_interest );
-			WGA_Plugin::app()->updatePopulateOption( 'ga_track_admin', $old_plugin_sga_render_when_loggedin );
-
-			$wpdb->query( "DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE 'sga_%';" );
-		}
-
-		WGA_Plugin::app()->updatePopulateOption( 'is_migrate_up_to_230', 1 );
-	}
-}
-
-add_action( 'init', 'wbcr_ga_upgrade' );
-
-/**
  * After saving the settings in the plugin, we check whether the
  * options for analytic caching are enabled or not. If enabled,
  * add cron task.
@@ -128,6 +48,84 @@ add_action( 'wbcr_factory_000_imppage_after_form_save', function ( $plugin, $pag
 }, 10, 2 );
 
 if ( ! defined( 'LOADING_GA_CACHE_AS_ADDON' ) ) {
+	/**
+	 * Notice that the plugin has been seriously updated!
+	 *
+	 * @since 3.0.1
+	 *
+	 * @param string $plugin_name
+	 *
+	 * @param array  $notices   all registered notices
+	 *
+	 * @return array all notices
+	 */
+	function wbcr_ga_admin_conflict_notices_error( $notices, $plugin_name ) {
+		if ( $plugin_name != WGA_Plugin::app()->getPluginName() ) {
+			return $notices;
+		}
+
+		$text = '<p>' . __( 'The <b>Simple Google Analytics</b> plugin has some major changes!', 'simple-google-analytics' ) . '</p>';
+		$text .= '<p>' . __( 'Unfortunately, the old version of the plugin (2.2.2) is no longer supported, but you still can download it from the WordPress repository in case if the new release doesn’t work for you.', 'simple-google-analytics' ) . '</p>';
+		$text .= '<p>' . __( 'We’ve updated the code and fixed the compatibility issue for the latest WordPress and PHP versions. We’ve also added additional feature of the Local Google Analytics – this way your website will load faster. The plugin’s name has been changed to Local Google Analytics, but all features remained the same.', 'simple-google-analytics' ) . '</p>';
+		$text .= '<p>' . sprintf( __( 'Please, check <a href="%s">plugin settings</a> and its performance on your website. We do care about you and want to avoid any problems with the new version.', 'simple-google-analytics' ) . '</p>', admin_url( 'options-general.php?page=ga_cache-' . WGA_Plugin::app()->getPluginName() ) ) . '</p>';
+		$text .= '<p>' . sprintf( __( 'We are aimed to pay more attention to the speed and security aspects of your website. That’s why you should definitely try our basic WordPress optimization plugin as well. Clearfy includes functionality of this plugin and has many additional features for the website optimization:
+<a href="%s">Donwload Clearfy for free</a>', 'simple-google-analytics' ), 'https://clearfy.pro?utm_source=wordpress.org&utm_campaign=' . WGA_Plugin::app()->getPluginName() ) . '</p>';
+
+		$notices[] = [
+			'id'              => 'ga_plugin_upgrade_notice1',
+			'type'            => 'warning',
+			'dismissible'     => true,
+			'dismiss_expires' => 0,
+			'text'            => $text
+		];
+
+		return $notices;
+	}
+
+	add_filter( 'wbcr/factory/admin_notices', 'wbcr_ga_admin_conflict_notices_error', 10, 2 );
+
+	/**
+	 * Migrate settings from the old plugin to the new one.
+	 *
+	 * @since 3.0.1
+	 * @return void
+	 */
+	add_action( 'init', function () {
+		global $wpdb;
+
+		if ( defined( 'LOADING_GA_CACHE_AS_ADDON' ) ) {
+			return;
+		}
+
+		$is_migrate_up_to_230 = WGA_Plugin::app()->getPopulateOption( 'is_migrate_up_to_230', false );
+
+		if ( ! $is_migrate_up_to_230 ) {
+			$old_plugin_tracking_id              = get_option( 'sga_analytics_id' );
+			$old_plugin_code_location            = get_option( 'sga_code_location' );
+			$old_plugin_demographic_and_interest = (int) get_option( 'sga_demographic_and_interest' );
+			$old_plugin_sga_render_when_loggedin = (int) get_option( 'sga_render_when_loggedin' );
+
+			if ( ! empty( $old_plugin_tracking_id ) ) {
+				WGA_Plugin::app()->updatePopulateOption( 'ga_cache', 1 );
+				WGA_Plugin::app()->updatePopulateOption( 'ga_tracking_id', $old_plugin_tracking_id );
+
+				$script_position = 'footer';
+
+				if ( $old_plugin_code_location == 'head' ) {
+					$script_position = 'header';
+				}
+
+				WGA_Plugin::app()->updatePopulateOption( 'ga_script_position', $script_position );
+				WGA_Plugin::app()->updatePopulateOption( 'ga_anonymize_ip', $old_plugin_demographic_and_interest );
+				WGA_Plugin::app()->updatePopulateOption( 'ga_track_admin', $old_plugin_sga_render_when_loggedin );
+
+				$wpdb->query( "DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE 'sga_%';" );
+			}
+
+			WGA_Plugin::app()->updatePopulateOption( 'is_migrate_up_to_230', 1 );
+		}
+	} );
+
 	/**
 	 * Add a link to plugin meta. You can find this link in admin panel on
 	 * the page plugins.php. Look under plugin short description.
@@ -165,7 +163,7 @@ if ( ! defined( 'LOADING_GA_CACHE_AS_ADDON' ) ) {
 	 * @return string
 	 */
 	add_filter( 'wbcr_factory_imppage_rating_widget_url', function ( $page_url, $plugin_name ) {
-		if ( ! defined( 'LOADING_GA_CACHE_AS_ADDON' ) && ( $plugin_name == WGA_Plugin::app()->getPluginName() ) ) {
+		if ( $plugin_name == WGA_Plugin::app()->getPluginName() ) {
 			return 'https://wordpress.org/support/plugin/simple-google-analytics/reviews/#new-post';
 		}
 
